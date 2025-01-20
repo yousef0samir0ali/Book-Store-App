b7f98f10c1c0791095089183d8c46812fde8a2c7
yousef0samir0ali <yao245.samir@gmail.com>
2025-01-20 22:30:20 +0300

    Update vite.config.js to fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Book-Store-App/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
-    base: "/Book-Store-App/",
   },
 });
